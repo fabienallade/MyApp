f99054af0c5a354e122095d16d7559acb32462d7
allade fabien <alladefabien@gmail.com>
2018-10-10 16:37:41 +0100

    comit debut
    debut de commit depuis un certain temps

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,18 +6,38 @@ class TodosController < ApplicationController
    @todo=Todo.new(todo_params)
    if @todo.save
      flash[:notice]="Le trucs a ete bien ajouter comme il le fallait"
-     redirect_to todos_path @todo
+     redirect_to todo_path @todo
    else
      render 'new'
    end
   end
+  def index
+     @todo=Todo.all
+  end
+  def destroy
+    @todo=Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] ="le travail aete bien fait"
+    redirect_to todos_path
+  end
+
   def show
-    if params[:format]
-      @todo=Todo.find(params[:format])
+      @todo=Todo.find(params[:id])
+  end
+
+  def edit
+    @todo=Todo.find(params[:id])
+  end
+  def update
+    @todo=Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice]="le travail a ete bien fait mon amie tu croyais quoi meme depuis le debut"
+      redirect_to todos_path(@todo)
     else
-      @todo=Todo.all
+      render "edit"
     end
   end
+
   private
   def todo_params
     params.require(:todo).permit(:name,:description)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "user#Hello"
   get "/about",to: "user#about"
-  resource :todos
+  resources :todos
 end
