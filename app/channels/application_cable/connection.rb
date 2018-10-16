9e73997eb9ace7572babf439178109c43d7d2f93
allade fabien <alladefabien@gmail.com>
2018-10-16 13:20:04 +0100

    16/10/2018/1

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,6 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+=begin
     identified_by current_user
     def connect
     self.current_user=find_current_user
@@ -15,5 +16,6 @@ module ApplicationCable
       reject_unauthorized_connection
     end
     end
+=end
   end
 end
